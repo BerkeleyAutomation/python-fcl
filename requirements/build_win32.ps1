440f57d591ceba866f4915e8df6f89e88ebadc5b
Pebaz <pebazium@gmail.com>
2020-01-13 19:32:54 -0500

    Windows Build Script and Associated Documentation Per Platform

---
+++ requirements/build_win32.ps1
@@ -0,0 +1,129 @@
+<#
+This script builds fcl and it's dependencies for python-fcl on Windows.
+
+It downloads, builds, installs, and then deletes:
+ * fcl
+ * libccd
+ * eigen
+ * octomap
+#>
+
+# Ensure that paths are constant between runs
+pushd $PSScriptRoot
+
+# Create a directory that encapsulates all Git repos
+mkdir fcl; cd fcl
+
+
+#------------------------------------------------------------------------------
+# Eigen
+Write-Host "Building Eigen"
+git clone https://gitlab.com/libeigen/eigen.git
+cd eigen
+mkdir build; cd build
+cmake -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 16 2019" -DBUILD_SHARED_LIBS=ON ..
+cmake --build . --config Release --target install
+Write-Host "Done"
+cd ../..
+
+
+# ------------------------------------------------------------------------------
+# LibCCD
+Write-Host "Building LibCCD"
+git clone https://github.com/danfis/libccd
+cd libccd
+mkdir build; cd build
+cmake -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 16 2019" -DBUILD_SHARED_LIBS=ON ..
+cmake --build . --config Release --target install
+Write-Host "Done"
+cd ../..
+
+# FCL won't be able to find libccd unless it is named "ccd" exactly
+Rename-Item "C:\Program Files (x86)\libccd" ccd
+
+
+# ------------------------------------------------------------------------------
+# Octomap
+Write-Host "Building Octomap"
+git clone https://github.com/OctoMap/octomap
+cd octomap
+mkdir build; cd build
+cmake -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 16 2019" -DBUILD_SHARED_LIBS=ON ..
+
+# Build Octomap (won't install for some reason)
+cmake --build . --config Release --target install
+
+# Now rebuild, which installs correctly
+cmake --build . --config Release --target install
+Write-Host "Done"
+cd ../../
+
+# FCL won't be able to find octomap unless it is named "octomap" exactly
+Rename-Item "C:\Program Files (x86)\octomap-distribution" octomap
+
+
+# ------------------------------------------------------------------------------
+# FCL
+Write-Host "Building FCL"
+git clone https://github.com/flexible-collision-library/fcl
+cd fcl
+
+# Checkout specific version 0.5.0 (only one compatible with python-fcl)
+git checkout 7075caf32ddcd5825ff67303902e3db7664a407a
+mkdir build; cd build
+
+# Fiddle with build file to help fcl find LibCCD and Octomap
+# Also tells compiler to dynamically link with C runtime (matches Cython)
+$build_script_modification = @"
+find_package(ccd QUIET)
+find_package(octomap QUIET)
+set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreadedDLL$<$<CONFIG:Debug>:Debug>")
+"@
+$filePath = "../CMakeLists.txt"
+$lineNumber = 11
+$fileContent = Get-Content $filePath
+$fileContent[$lineNumber-1] = $build_script_modification
+$fileContent | Set-Content $filePath
+
+# Now perform actual build
+cmake -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 16 2019" ..
+cmake --build . --config Release --target install
+Write-Host "Done"
+cd ../../
+
+
+# ------------------------------------------------------------------------------
+# Python-FCL
+cd ../
+
+# Delete compilation directory since it is no longer needed
+del -Force -Recurse fcl
+
+cd ../
+
+# Now build Cython extension
+python setup.py install
+Write-Host "Successfully built python-fcl"
+
+# At this point, you cannot import fcl directly in Python because it will fail.
+# This is because it doesn't have the required DLLs. Let's fix that:
+# NOTE: The default SourceFileLoader will find python-fcl's `fcl` package. So
+# let's create a tmp directory and use that to force it to find the one in the
+# `site-packages`'s directory.
+mkdir tmp; cd tmp
+$fcl_dir = $(python -c "import pathlib as p, pkgutil as pk; print(p.Path(pk.get_loader('fcl').get_filename()).parent)")
+Write-Host "Installed into: $fcl_dir. Copying dependent DLLs"
+ls "C:/Program Files (x86)/octomap/bin/*.dll" | cp -destination $fcl_dir
+cp "C:/Program Files (x86)/ccd/bin/ccd.dll" $fcl_dir
+
+# Now delete all of the installed dependencies (after building python-fcl)
+Write-Host "Removing build directories for Eigen, LibCCD, FCL, and Octomap"
+rmdir -r 'C:\Program Files (x86)\Eigen3'
+rmdir -r 'C:\Program Files (x86)\ccd'
+rmdir -r 'C:\Program Files (x86)\fcl'
+rmdir -r 'C:\Program Files (x86)\octomap'
+
+Write-Host "All done!"
+
+# Make sure to return the cwd to wherever it was before the build
+popd
--- setup.py
+++ setup.py
@@ -1,48 +1,69 @@
-import os
-import sys
-import inspect
-
+import sys, os
+from pathlib import Path
 from setuptools import Extension, setup
+import numpy
 
 
-# get current directory of file in case someone
-# called setup.py from elsewhere
-cwd = os.path.dirname(os.path.abspath(
-    inspect.getfile(inspect.currentframe())))
+# Load __version__
+exec((Path(__file__).parent / 'fcl/version.py').read_text())
 
-# load __version__
-exec(open(os.path.join(cwd,
-                       'fcl/version.py'), 'r').read())
-    
-platform_supported = False
-for prefix in ['darwin', 'linux', 'bsd']:
-    if prefix in sys.platform:
-        platform_supported = True
-        include_dirs = ['/usr/include',
-                        '/usr/local/include',
-                        '/usr/include/eigen3']
-        lib_dirs = ['/usr/lib',
-                    '/usr/local/lib']
-        
-        if 'CPATH' in os.environ:
-            include_dirs += os.environ['CPATH'].split(':')
-        if 'LD_LIBRARY_PATH' in os.environ:
-            lib_dirs += os.environ['LD_LIBRARY_PATH'].split(':')
+# Libraries needed during compilation of extension
+libraries = ['fcl', 'octomap']
 
-        try:
-            # get the numpy include path from numpy
-            import numpy
-            include_dirs.append(numpy.get_include())
-        except:
-            pass
+# Customize build options per platform
+supported_platforms = 'darwin', 'linux', 'bsd', 'win32'
+current_platform = sys.platform
 
-        break
+if current_platform in supported_platforms:
 
-if sys.platform == "win32":
-    platform_supported = False
+    # Extra compile args needed per platform
+    compile_args = []
 
-if not platform_supported:
-    raise NotImplementedError(sys.platform)
+    # Windows include and library search paths
+    if current_platform == 'win32':
+        # Run build script to download and build libraries.
+
+        include_dirs = []
+        library_dirs = []
+        include_dir = r'C:\Program Files (x86)\{}\include'
+        library_dir = r'C:\Program Files (x86)\{}\lib'
+        deps = 'ccd', 'octomap', 'fcl'
+
+        for dep in deps:
+            include_dirs.append(include_dir.format(dep))
+            library_dirs.append(library_dir.format(dep))
+
+        # Add necessary Windows-specific dependencies
+        libraries.extend(['octomath', 'ccd', 'vcruntime'])
+
+        # Extra args for MSVC
+        compile_args.extend(['/std:c++latest', '/MD', '/w'])
+
+    # Unix include and library search paths
+    else:
+        include_dirs = [
+            '/usr/include',
+            '/usr/local/include',
+            '/usr/include/eigen3'
+        ]
+        library_dirs = [
+            '/usr/lib',
+            '/usr/local/lib'
+        ]
+
+        # Extra args for CC
+        compile_args.append('-std=c++11')
+
+    # Optionally support additional search paths
+    sep = os.path.pathsep
+    include_dirs.extend(os.environ.get('CPATH', '').split(sep))
+    include_dirs.extend(os.environ.get('LD_LIBRARY_PATH', '').split(sep))
+
+    # Add Numpy include directories
+    include_dirs.append(numpy.get_include())
+
+else:
+    raise NotImplementedError(current_platform)
 
 setup(
     name='python-fcl',
@@ -52,7 +73,7 @@ setup(
     url='https://github.com/BerkeleyAutomation/python-fcl',
     author='Matthew Matl',
     author_email='mmatl@eecs.berkeley.edu',
-    license = "BSD",
+    license = 'BSD',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: BSD License',
@@ -73,14 +94,15 @@ setup(
     setup_requires=['cython'],
     install_requires=['numpy', 'cython'],
     ext_modules=[Extension(
-        "fcl.fcl",
-        ["fcl/fcl.pyx"],
-        include_dirs = include_dirs,
-        library_dirs = lib_dirs,
-        libraries=[
-                "fcl","octomap"
-                ],
-        language="c++",
-        extra_compile_args = ["-std=c++11"]
+        'fcl.fcl',
+        ['fcl/fcl.pyx'],
+        include_dirs=include_dirs,
+        library_dirs=library_dirs,
+        libraries=libraries,
+        language='c++',
+        extra_compile_args=compile_args
     )]
 )
+
+# On Windows, DLL dependencies must be copied over to the installation directory
+
